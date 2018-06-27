9c15b53c31b60481561d37b2f30e37fd44b9cee0
Michael Potter <mgp140@gmail.com>
2018-06-27 12:02:44 -0400

    started on the selectable component

--- Makefile
+++ Makefile
@@ -9,4 +9,4 @@ clean:
 	docker-compose down --remove-orphans -v
 
 getboxfiles:
-	cp /Users/$(USER)/Box\ Sync/b-odl\ Shared/Chemical\ Reaction\ Lab/ChemRoom.dae ./assets/ChemRoom.dae
+	cp /Users/$(USER)/Box\ Sync/b-odl\ Shared/Chemical\ Reaction\ Lab/ChemRoom.dae ./assets/chemlablayout.gltf
---
+++ selectable-component.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent('selectable', {
+  /**
+   * Initial creation and setting of the mesh.
+   */
+  init: function () {
+    this.__selected = false;
+    // toggle internal state of selected
+    this.el.addEventListener('mousedown', e => {
+      this.__selected = !this.__selected;
+      console.log('mousedown');
+    })
+  },
+
+})
