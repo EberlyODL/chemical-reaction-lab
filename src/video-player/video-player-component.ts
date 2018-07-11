e8a73756868e99212b7250c74a9d46fa8dac4fff
heyMP <mgp140@gmail.com>
2018-07-11 13:27:57 -0400

    started on video

--- src/video-player/video-player-component.ts
+++ src/video-player/video-player-component.ts
@@ -9,10 +9,25 @@ const register = () => {
     },
 
     init: function () {
+      const matrix = [
+        { id: 3, combination: ['CuOH2', 'bunsen_burner']},
+        { id: 7, combination: ['magnesiumstrips', 'bunsen_burner']},
+        { id: 9, combination: ['3MHCl', 'CuOH2']},
+      ]
       this.subscribe = store.subscribe(() => {
         const state = store.getState()
-        const selectedItems = state.selectedItems
-        // find out if the combination is available
+        const selectedItems:any = state.selectedItems
+        console.log(selectedItems)
+        const activeCombination = matrix.find((i:any) => {
+          let includes = true;
+          i.combination.forEach((e:any) => {
+            if (!selectedItems.includes(e)) {
+              includes = false
+            }
+          });
+          return includes
+        })
+        console.log(activeCombination)
       })
     },
 
