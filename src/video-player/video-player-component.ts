720a6577a2a58e94ee4f47e12fe714947e5a92de
cgldevel <cgldevel@gmail.com>
2018-07-12 12:07:10 -0400

    added video assets and wired them to scene

--- src/video-player/video-player-component.ts
+++ src/video-player/video-player-component.ts
@@ -10,10 +10,16 @@ const register = () => {
 
     init: function () {
       this._matrix = [
-        // { id: 3, combination: ['CuOH2', 'bunsen_burner'] },
-        // { id: 7, combination: ['magnesiumstrips', 'bunsen_burner'] },
-        // { id: 9, combination: ['3MHCl', '3MNaOH'], video: '#reagentvid-3' },
-        { id: 17, combination: ['03MAgNO3', '01MNaBr'], video: '#reagentvid-17'}
+        { id: 3, combination: ['CuOH2', 'bunsenburner'], video: '#reagentvid-3'},
+        { id: 7, combination: ['magnesiumstrips', 'bunsenburner'], video: '#reagentvid-7'},
+        { id: 9, combination: ['3MHCl', '1MNa2CO3'], video: '#reagentvid-9'},
+        { id: 10, combination: ['', 'copperstrips'], video: '#reagentvid-10'},
+        { id: 13, combination: ['013CuSO4', 'magnesiumstrips'], video: '#reagentvid-13'},
+        { id: 14, combination: ['01MCaCl2', '01MNa2CO3'], video: '#reagentvid-14'},
+        { id: 15, combination: ['3MHCl', '3MNaOH'], video: '#reagentvid-15'},
+        { id: 16, combination: ['03MAgNO3', '01MCaCl2'], video: '#reagentvid-16'},
+        { id: 17, combination: ['03MAgNO3', '01MNaBr'], video: '#reagentvid-17'},
+        { id: 18, combination: ['03MAgNO3', '01MKI'], video: '#reagentvid-18'},
       ]
       // check the store and subscribe to any changes
       this.__storeChanged()
