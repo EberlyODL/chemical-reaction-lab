31ef2c88cd448f277170612beda4d1aa332305dd
heyMP <mgp140@gmail.com>
2018-11-13 21:44:47 -0500

    updated for publishing

--- packages/frontend/src/active-item-indicator/active-item-indicator.js
+++ packages/frontend/src/active-item-indicator/active-item-indicator.js
@@ -35,6 +35,7 @@ AFRAME.registerComponent("active-item-indicator", {
     this.el.setAttribute("visible", false);
 
     this.el.sceneEl.addEventListener("droppable-surface-active-item-added", e => {
+      console.log('e', e);
       this.updateIndicator(e.detail)
     });
 
